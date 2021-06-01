a51daabc3761c77324dad1059a8431b51b38ff45
jaytohe <antolbestg4@gmail.com>
2021-06-01 14:08:04 +0100

    - Reverted TicketClass to non-static regular class.
    - Added closeConnection to close the DB connection.
    
    - Added the full dir path when connecting to the DB.
    
    - Updated the tests with to reflect the class changes.

--- backend/ticket_manager.py
+++ backend/ticket_manager.py
@@ -3,58 +3,58 @@ import pyqrcode
 import png
 import uuid
 import sqlite3
+import os
 
 class TicketClass:
-    MAX_LIMIT_TICKETS: int = 50 #store max number of tickets.
 
-    @staticmethod
-    def createRandomHash() -> str:
+    def __init__(self):
+        DB_path: str = f"{os.getcwd()}\issued_tickets.db" 
+        self.conn: Connection = sqlite3.connect(DB_path)
+        self.cursor: Cursor = self.conn.cursor()
+        self.MAX_LIMIT_TICKETS: int = 50 #store max number of tickets.
+
+    def createRandomHash(self) -> str:
         return uuid.uuid4().hex
 
-    @staticmethod
-    def setMaxTickets(num: int) -> None: #set the max amount of tickets.
-        if num > 0:
-            MAX_LIMIT_TICKETS = num
 
-    @staticmethod
-    def setCheckedIn(tid: str, checkedIn: int =1) -> bool: #Set Check-In status given a valid ticked id; returns true if update was succesful. False on any error.
-        conn = sqlite3.connect("issued_tickets.db")
+    def setMaxTickets(self, num: int) -> None: #set the max amount of tickets.
+        if (num > 0):
+            self.MAX_LIMIT_TICKETS = num
+    
+    def closeConnection(self) -> None: #closes the database connection.
+        self.conn.close()
+        self.conn = None
+
+    def setCheckedIn(self, tid: str, checkedIn: int =1) -> bool: #Set Check-In status given a valid ticked id; returns true if update was succesful. False on any error.
         try:
-            with conn:
-                conn.execute("UPDATE Tickets SET CheckedIn=? WHERE TicketID=?",(checkedIn, tid))
+            with self.conn:
+                self.conn.execute("UPDATE Tickets SET CheckedIn=? WHERE TicketID=?",(checkedIn, tid))
         except Exception as ex:
             print(str(ex))
             return False
-        finally:
-            conn.close()
-
+        
         return True
 
-    @staticmethod
-    def createQRCode(url: str, db_values: tuple) -> str: #adds a new ticket to the db and returns a qr code for it.
-        randomID: str = TicketClass.createRandomHash()
+
+    def createQRCode(self, url: str, db_values: tuple) -> str: #adds a new ticket to the db and returns a qr code for it.
+        randomID: str = self.createRandomHash()
         url += randomID
         qr_encoded_url = pyqrcode.create(url)
-        con = sqlite3.connect("issued_tickets.db")
         try:
-            if(TicketClass.getNumSoldTickets() >= TicketClass.MAX_LIMIT_TICKETS):
-                raise Exception("Sold Out!")
-            with con:
-                con.execute("INSERT INTO Tickets(FirstName, LastName,TicketID) VALUES(?,?,?)",(db_values[0], db_values[1],randomID))
+            if(self.getNumSoldTickets() >= self.MAX_LIMIT_TICKETS):
+                raise Exception("MAX_LIMIT_TICKETS reached.")
+            with self.conn:
+                self.conn.execute("INSERT INTO Tickets(FirstName, LastName,TicketID) VALUES(?,?,?)",(db_values[0], db_values[1],randomID))
         except Exception as ex:
-            print(f"Smth went wrong! {str(ex)}")
+            print(str(ex))
             return ""
-        finally:
-            con.close()
 
         return f"data:image/png;base64,{qr_encoded_url.png_as_base64_str(scale=5, quiet_zone=6)}"
 
-    @staticmethod
-    def verify_ticket(tid: str) -> Union[bool,tuple]: #verifies if a ticket id is valid i.e. present in the DB.
-        conn = sqlite3.connect("issued_tickets.db")
-        cursor = conn.cursor()
-        cursor.execute("SELECT FirstName,LastName,CheckedIn FROM Tickets WHERE TicketID=?", (tid,))
-        record: tuple = cursor.fetchone()
+
+    def verify_ticket(self, tid: str) -> Union[bool,tuple]: #verifies if a ticket id is valid i.e. present in the DB.
+        self.cursor.execute("SELECT FirstName,LastName,CheckedIn FROM Tickets WHERE TicketID=?", (tid,))
+        record: tuple = self.cursor.fetchone()
 
         entry_info = None
 
@@ -69,20 +69,16 @@ class TicketClass:
         if entry_info is None:
             entry_info = (False, ("",""))
 
-        conn.close()
         return entry_info
 
-    @staticmethod
-    def getNumSoldTickets() -> int: #returns the number of tickets sold.
-        conn = sqlite3.connect("issued_tickets.db")
-        cursor = conn.cursor()
-        cursor.execute("SELECT COUNT(TicketID) FROM Tickets")
-        sold: int = cursor.fetchone()[0]
+
+    def getNumSoldTickets(self) -> int: #returns the number of tickets sold.
+        self.cursor.execute("SELECT COUNT(TicketID) FROM Tickets")
+        sold: int = self.cursor.fetchone()[0]
 
         if sold is None:
             return 0
 
-        conn.close()
         return sold
 
 
@@ -95,9 +91,10 @@ if __name__ == "__main__":
     ("John", "Doe"),
     ("Bob", "White")
     )
-
+    
+    ticketmgr = TicketClass()
     for entry in test_entries:
-        qr_code_base64 = TicketClass.createQRCode("https://example.com?id=", entry)
+        qr_code_base64 = ticketmgr.createQRCode("https://example.com?id=", entry)
         if qr_code_base64 != "":
             print(entry)
             print(qr_code_base64)
@@ -107,21 +104,23 @@ if __name__ == "__main__":
 
     TestID1 = "<Test a valid ID here>"
     print(f"Verifying {TestID1}")
-    print(TicketClass.verify_ticket(TestID1))
+    print(ticketmgr.verify_ticket(TestID1))
 
     print()
 
     print("Verifying ILikeSpaghettiSomuch0799ak95jdl8")
-    print(TicketClass.verify_ticket("ILikeSpaghettiSomuch0799ak95jdl8"))
+    print(ticketmgr.verify_ticket("ILikeSpaghettiSomuch0799ak95jdl8"))
 
     print()
 
-    print(TicketClass.setCheckedIn("<Test a CheckedIn ID Here>"))
+    print(ticketmgr.setCheckedIn("<Test a CheckedIn ID Here>"))
 
     TestID2 ="<Test a CheckedIn ID Here>"
     print(f"Verifying {TestID2}")
-    print(TicketClass.verify_ticket(TestID2))
+    print(ticketmgr.verify_ticket(TestID2))
 
     print()
 
-    print(f"Tickets Sold: {TicketClass.getNumSoldTickets()}")
+    print(f"Tickets Sold: {ticketmgr.getNumSoldTickets()}")
+    
+    ticketmgr.closeConnection()
