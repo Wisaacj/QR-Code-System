8b47725074a354c2a61eb8032f0a8fea5c36f880
jaytohe <antolbestg4@gmail.com>
2021-06-01 02:12:43 +0100

    Cleaned up create_db off of unused sql code

--- backend/create_db.py
+++ backend/create_db.py
@@ -2,8 +2,8 @@ import sqlite3
 conn = sqlite3.connect('issued_tickets.db')
 cursor = conn.cursor()
 
-create_table_queries = (
-'''
+
+query = '''
 CREATE TABLE Tickets (
     FirstName TEXT NOT NULL,
     LastName TEXT,
@@ -11,19 +11,10 @@ CREATE TABLE Tickets (
     CheckedIn INT DEFAULT 0,
     PRIMARY KEY (TicketID)
 )
-''',
-
 '''
-CREATE TABLE Other (
-    OtherData TEXT
-)
-'''
-)
 
-for query in create_table_queries:
-    conn.execute(query)
-    conn.commit()
-
+conn.execute(query)
+conn.commit()
 conn.close()
     
 
