471bcab8afb133b6111b5a199b17c3718c6be333
Wisaacj <wisaacj@live.co.uk>
2021-06-02 17:53:39 +0100

    Completed Payment Method

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from backend.ticket_manager import TicketClass
+
+import stripe
 
 app = Flask(__name__)
 
+# Adding STRIPE tokens to the app configuration
+app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_1CPqKqHEMYPL3pep8bzUadZQ'
+app.config['STRIPE_SECRET_KEY'] = 'sk_test_2dqyTdYE3bmQbslK8JQg3X9D'
+
+# Setting up the STRIPE API key
+stripe.api_key = app.config['STRIPE_SECRET_KEY']
+
 # Route for the ticket purchasing page
 @app.route('/', methods=['GET', 'POST'])
-def welcome_page():
+def index():
     error = None
 
     # Getting the number of tickets remaining to be sold
@@ -13,25 +22,49 @@ def welcome_page():
 
     # Returning an error message if all the tickets have been sold
     if (tickets_remaining == 0):
+        # Need to deactivate the fields and button
         return render_template('/tickets/buy-tickets.html', error="No tickets remaining, better luck next time", tickets_remaining=tickets_remaining)
 
+    # When there's a POST request, get the data from the form
     if (request.method == "POST"):
-        """
-        - Use a paypal API to fufil the payment payment
-        - Check if the payment was successfull
-        - If so, continue and make the ticket
-        """
-        # Creating a new QR code for the user's ticket
-        qr_code = TicketClass.createQRCode("127.0.0.1:5000/verify?id=", (request.form['floatingForename'], request.form['floatingSurname']))
-        if (qr_code != ""):
-            # Purchase was successful
-            fullname = request.form['floatingForename'] + " " + request.form['floatingSurname']
-            return render_template('/tickets/success.html', name=fullname, qr_code=qr_code)
-        else:
-            error = "Unsuccessful operation, please try again"
+        forename = request.form['floatingForename']
+        surname = request.form['floatingSurname']
+        email = request.form['floatingInput']
 
-    return render_template('/tickets/buy-tickets.html', error=error, tickets_remaining=tickets_remaining)
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price': 'price_1Ixv2UDTPk3QaJIM1e6YzLrw',
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=url_for('success_page', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url=url_for('index', _external=True) + '?error=Fail',
+    )
 
+    return render_template('/tickets/buy-tickets.html',
+        checkout_session_id=session['id'],
+        checkout_public_key=app.config['STRIPE_PUBLIC_KEY'],
+        error=error, 
+        tickets_remaining=tickets_remaining
+    )
+
+@app.route('/success', methods=['GET'])
+def success_page():
+    # Purchase was successful
+    session = stripe.checkout.Session.retrieve(request.args.get('session_id'))
+    customer = stripe.Customer.retrieve(session.customer)
+
+    fullname = customer.name
+    if (len(fullname.split(" ")) > 1):
+        forename, surname = fullname.split(" ")[0], fullname.split(" ")[1]
+    else:
+        forename, surname = fullname, ""
+
+    # Creating a new QR code for the user's ticket
+    qr_code = TicketClass.createQRCode("127.0.0.1:5000/verify?id=", (forename, surname))
+    
+    return render_template('/tickets/success.html', name=fullname, qr_code=qr_code)
 
 @app.route('/verify')
 def verification_page():
