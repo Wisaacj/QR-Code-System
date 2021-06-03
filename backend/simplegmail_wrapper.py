4b085b131c5ab6c79db12ac15f678a95b1cb7514
jaytohe <antolbestg4@gmail.com>
2021-06-04 00:45:19 +0100

    Generalized for github

--- backend/simplegmail_wrapper.py
+++ backend/simplegmail_wrapper.py
@@ -15,14 +15,14 @@ class GreetingEmail:
         with open('QR_TICKET.png', 'wb') as f:
             f.write(image)
             
-    def generate_email(self):
-        self.email= self.email_template.format(name=self.name, surname=self.surname, br="<br>")
+    def generate_email(self) -> None:
+        self.email: str = self.email_template.format(name=self.name, surname=self.surname, br="<br>")
     
-    def send(self):
+    def send(self) -> None:
         gmail_connector = Gmail()
         params: dict = {
         "to": self.emailaddr, 
-        "sender": "antolbestg4@gmail.com",
+        "sender": "<youremail>@gmail.com",
         "subject": "Your Big Bertha Ticket",
         "msg_html": self.email,
         "attachments": ["QR_TICKET.png"],
@@ -39,7 +39,7 @@ class GreetingEmail:
 if __name__ == "__main__": #run tests
     test_qrcode:str = "iVBORw0KGgoAAAANSUhEUgAAAQkAAAEJAQAAAACvE+/JAAACnElEQVR4nO2ZS26mMBCEG7FgyRG4ibkYEkhcDG7iI7BkgdJTVT2RIJOZ2bYUvLAM/n4pjftR7Zj/byz2Ii/yIj8RuQxjGqzBY+eHaRr4ckiJYHVMdo7utfvo8QusvIuNjIg1BywaYcdp/QIYq26DlWmRGSZggjGrW+lhUXIEFrWcsLH/xaIkCP0F7/q10lU4XcM3LpUEiWjs9uM+fRewKRCNy/zDWixwCp/GaCREFI3ykrNhmis6iuNuUR7ksrPxtZ4WFk3muy9xCikRZDhWEusRl8UQl8UmQ2FZPSXCR3DMG2chh28P9+k9JYI0vCs147OrkpjKSXlEYx7kskhzhbsNOSbpZ2pOhHAcM8NvroDh7dZ7RVy2nhFB1YBv1N8rejY9hwYOGRFWuqOtkpdQbouC03QKKRGnvKT2oQAyOTpWdrcoEcLkERqzDSkEY2a6981fkiFYqZLQoqqs3O3h+OkQtBkbNhB+mOAvIYpOHkVGRNkCG3x3NhGca+1YrDMiHFSW2HAoCgQnbWP+84yI/GWtrsazMDXL26kyciJIyAi/QJjmVEnGh0V5EGeJxl+/GdUwfBsqg3X6ZlEm5FLeqKdyMVOGqZI8NWYehBLCpYYnNhwKyVBuQ0oErsLPLg3MkMRq+ezu8iEQ77JoDLWp/gM1pXm0/5kQJLc2kocEUMeeDudxtygT4lt4Nu9LmDJ2KuRyj8ZUiPojcHB0dkVyH/ui4NMgGjJGWfka1NvrZiIjonupmTpCJVDXaLyKiN/mQ4aQEOzkJDDU5eNxTYrEvdTGTk5dx8hobB4XPdmQyBanrib3EMdf/x+QCYkLCH52tfUVhe+PA0iCyF+Unzu+ZRHRbUpOJKIx+qPSS8OtLoGREfn3eJEXeZGfh/wCqy2NwMLyUm4AAAAASUVORK5CYII="
     
-    test = GreetingEmail("Bob", "White", "anthony.dalamagas@gmail.com", test_qrcode)
+    test = GreetingEmail("Bob", "White", "<receiver_email>@gmail.com", test_qrcode)
     
     a = test.send()
     if (a):
